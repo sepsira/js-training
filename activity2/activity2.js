7a9c4049f6aedbfb172eb9199cca8c6cb40f2bf1
Ela S <u23.277@gmail.com>
2025-10-22 22:44:18 -0700

    Add comment for sentence formatting function
    Add a comment explaining the purpose of the function.

--- activity2/activity2.js
+++ activity2/activity2.js
@@ -1,1 +1,9 @@
+/*
+take the three words inputted, format them to look like a gramatically correct
+sentence (first word capitalized, punctuation, spaces), and output the
+completed sentence.
+*/
 
+(function() {
+    // write here!
+}) ();
