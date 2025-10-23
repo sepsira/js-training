4b4018684f38d6581f054b6f314d7964096fbc71
Ela S <u23.277@gmail.com>
2025-10-22 21:58:00 -0700

    Add comments instructions to activity1.js
    Added instructions for implementing comment submission functionality.

--- activity1/activity1.js
+++ activity1/activity1.js
@@ -1,5 +1,15 @@
-// you gotta make this!
+/*
+your goal is to code this such that when a comment is submitted, the text
+entered shows up on the right hand side.
 
+consider looking at the documentation for getting and setting values, and
+event listeners!
+
+(hint)
+document.getElementById("") is something we use a LOT, get used to it
+*/
+
+// this function will run itself when your code runs
 (function() {
     // write here!
 }) ();
